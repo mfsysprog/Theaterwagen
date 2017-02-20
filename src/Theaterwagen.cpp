d3d1aebb4e0edddd0f9330941711853a11bd95e0
Erik Janssen <eaw.janssen@chello.nl>
2017-02-20 02:43:15 -0800

    Eerste commit

---
+++ src/Theaterwagen.cpp
@@ -0,0 +1,15 @@
+   //============================================================================
+// Name        : Theaterwagen.cpp
+// Author      : Erik Janssen
+// Version     :
+// Copyright   : MIT License
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
